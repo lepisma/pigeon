2dbb3e2de7835b2cbf59852f14ecf47d414edb51
Abhinav Tushar <lepisma@fastmail.com>
2018-09-19 05:02:07 +0530

    Better transformer

--- src/pigeon.lisp
+++ src/pigeon.lisp
@@ -1,12 +1,18 @@
 (in-package #:pigeon)
 
-(defun transform (input-text)
-  (fmt (read-from-string input-text)))
+(defun read-code (input-path)
+  (with-open-file (fp input-path)
+    (let ((forms (loop for form = (read fp nil)
+                       while form collect form)))
+      (cl-strings:join (mapcar #'fmt forms)
+                       :separator (make-string 2 :initial-element #\linefeed)))))
+
+(defun write-code (code output-path)
+  (with-open-file (fp output-path
+                      :direction :output
+                      :if-exists :overwrite
+                      :if-does-not-exist :create)
+    (format fp code)))
 
 (defun transform-file (input-path output-path)
-  (let ((code (transform (uiop:read-file-string input-path))))
-    (with-open-file (fp output-path
-                        :direction :output
-                        :if-exists :overwrite
-                        :if-does-not-exist :create)
-      (format fp code))))
+  (write-code (read-code input-path) output-path))
