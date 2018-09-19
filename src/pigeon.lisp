0553244ec0265abcd9c7ff6fb2272a2053649b7d
Abhinav Tushar <lepisma@fastmail.com>
2018-09-19 22:53:13 +0530

    Support loading macros in a .pgl file

--- src/pigeon.lisp
+++ src/pigeon.lisp
@@ -1,5 +1,16 @@
 (in-package #:pigeon)
 
+(defun load-pgl (input-path)
+  "Load a pgl file with macro definitions."
+  (with-open-file (fp input-path)
+    (loop for form = (read fp nil)
+          while form
+          do (match form
+               ((list* 'defmacro name _)
+                (eval form)
+                (setf *macros* (cons name *macros*)))
+               (form (eval form))))))
+
 (defun read-code (input-path)
   (with-open-file (fp input-path)
     (let ((forms (loop for form = (read fp nil)
