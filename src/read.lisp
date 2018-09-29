982d88945a92fad9ff6aca418cbb20123356111b
Abhinav Tushar <lepisma@fastmail.com>
2018-09-30 01:30:02 +0530

    reader macro for case sensitive python ids

--- src/read.lisp
+++ src/read.lisp
@@ -14,8 +14,17 @@
                 (setf *macros* (cons name *macros*)))
                (form (eval form))))))
 
+(defun read-case-sensitive-id (stream subchar arg)
+  (declare (ignore subchar arg))
+  (let ((name (read stream t nil t)))
+    `(cons 'id ,name)))
+
+(defun enable-case-sensitive-id-syntax ()
+  (set-dispatch-macro-character #\# #\i #'read-case-sensitive-id))
+
 (defun read-pg (input-path)
   "Read pigeon code forms"
+  (enable-case-sensitive-id-syntax)
   (with-open-file (fp input-path)
     (loop for form = (read fp nil)
           while form collect form)))
